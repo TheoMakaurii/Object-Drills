85c5bdd62c4970c7904e86e0392267a255ea5d4d
Theo McCauley <TheoMakaurii@gmail.com>
2020-08-07 14:40:20 -0400

    third drill complete'

--- object-drills.js
+++ object-drills.js
@@ -27,66 +27,17 @@ for (let element in band){
   console.log(element, band[element]);
 }
 
-/*
-// function testLoaf(){
-
-// }
-
-/*let band {
-  sound1 = 'foo',
-  sound2 = 'bar',
-  sound3 = 'fum',
-  sound4 = 'quux',
-  sound5 = 'spam',
-  
-
-}
-for (let sound1 in band){
-  console.log(`${sound1}: ${band[sound1]} `)
-}
-console.log
-// TRY THIS : 
-//for (let element in band){
-//  console.log(element, person[element])
-//}
-
-
-
-for(let element of people)
-  console.log(`${element['name']} ${element['jobTitle']}`);
-
-
-
-//console.log('sound1' in band) // -> foo
-
-let people = [
-  {
-    name: "Dora",
-    jobTitle: "explorer",
-  },
-  {
-    name: "Bob",
-    jobTitle: "the builder",
-  },
-  {
-    name: "Mario",
-    jobTitle: "plumber",
-
-  }
+let  food= {
+  meals:
+  [
+    'breakfast', 
+    'second breakfast', 
+    'eleveneses', 
+    'lunch', 
+    'afterneoon tea',
+    'dinner',
+    'super supper'
 ]
-for (let i =0; i< people.length; i++)
-let person = people[i]
-console.log(person.name, person.jobTitle)
+}
 
-// (people[i].name, people[i].jobTitle)
-
-// this also works: people.forEach(person => console.log(person.name, person.jobTitle));
-
-/*
-EXAMPLE OF "for in" without using for Each
-this is like using our object in an array
-let sonic = people[0]
-console.log(sonice);
-for (let key in sonic){
-  console.log(key, sonic[key])
-}*/
+console.log(food.meals[3])
